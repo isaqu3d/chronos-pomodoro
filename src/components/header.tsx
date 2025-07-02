bbc1e19fdf7bb516855563277fad1ba63be8e506
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-02 09:09:29 -0300

    feat: create header component

---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import { Home, Settings, Sun, Timer } from "lucide-react";
+import { Logo } from "./logo";
+
+export function Header() {
+  return (
+    <div className="my-12">
+      <Logo />
+
+      <div className="flex justify-center gap-2 mt-8 ">
+        <button className="bg-green-600 p-2 rounded-md hover:bg-green-500 transition-colors cursor-pointer ">
+          <Home />
+        </button>
+        <button className="bg-green-600 p-2 rounded-md hover:bg-green-500 transition-colors cursor-pointer ">
+          <Timer />
+        </button>
+        <button className="bg-green-600 p-2 rounded-md hover:bg-green-500 transition-colors cursor-pointer ">
+          <Settings />
+        </button>
+        <button className="bg-green-600 p-2 rounded-md hover:bg-green-500 transition-colors cursor-pointer ">
+          <Sun />
+        </button>
+      </div>
+    </div>
+  );
+}
