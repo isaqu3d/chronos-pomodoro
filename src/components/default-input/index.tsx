41b200c19e5394c3dd140d864adf0f30b8dca9fd
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-10 14:12:51 -0300

    feat: refactor DefaultInput component for improved structure and styling

--- src/components/default-input/index.tsx
+++ src/components/default-input/index.tsx
@@ -3,11 +3,21 @@ type DefaultInputProps = {
   labelText: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-export function DefaultInput({ id, type, labelText }: DefaultInputProps) {
+export function DefaultInput({
+  id,
+  type,
+  labelText,
+  ...rest
+}: DefaultInputProps) {
   return (
     <>
       <label htmlFor={id}>{labelText}</label>
-      <input id={id} type={type} className="bg-red-700" />
+      <input
+        id={id}
+        type={type}
+        className="bg-transparent rounded-md p-2 outline-none border border-gray-600 text-white placeholder:text-gray-500 focus:border-green-700 transition-colors"
+        {...rest}
+      />
     </>
   );
 }
