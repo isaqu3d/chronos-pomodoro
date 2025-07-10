157a213d283eddcb77e07dc8730f74a33151c4f3
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-09 23:40:36 -0300

    feat: update CountDown component styling for improved visibility

--- src/components/count-down/index.tsx
+++ src/components/count-down/index.tsx
@@ -1,6 +1,6 @@
 export function CountDown() {
   return (
-    <div className="flex justify-center items-center text-green-700 hover:brightness-75 transition pt-14 ">
+    <div className="sm:text-9xl font-bold text-center text-white text-7xl ">
       00:00
     </div>
   );
