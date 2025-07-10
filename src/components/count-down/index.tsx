4a17335a45c9e1bef160845a5323ea04f946eef1
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-09 22:25:52 -0300

    feat: add CountDown component

---
+++ src/components/count-down/index.tsx
@@ -0,0 +1,7 @@
+export function CountDown() {
+  return (
+    <div className="flex justify-center items-center text-green-700 hover:brightness-75 transition pt-14 ">
+      00:00
+    </div>
+  );
+}
