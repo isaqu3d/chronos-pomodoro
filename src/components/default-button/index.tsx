70660bb48e6ec9a4bb80af976f2bb7be6db1d59f
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-14 17:35:24 -0300

    feat: enhance DefaultButton component with color variants and dynamic className support

--- src/components/default-button/index.tsx
+++ src/components/default-button/index.tsx
@@ -1,14 +1,33 @@
+import { tv } from "tailwind-variants";
+
 type DefaultButtonProps = {
   icon: React.ReactNode;
+  color?: "green" | "red";
+  className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function DefaultButton({ icon, ...rest }: DefaultButtonProps) {
+export function DefaultButton({
+  icon,
+  color = "green",
+  className,
+  ...rest
+}: DefaultButtonProps) {
+  const button = tv({
+    base: "text-gray-600 flex items-center justify-center p-1 max-w-48 w-screen rounded-lg cursor-pointer hover:bg-opacity-80 transition-colors duration-300 my-8",
+    variants: {
+      color: {
+        green: "bg-green-600 hover:bg-green-700",
+        red: "bg-red-600 hover:bg-red-700",
+      },
+    },
+    defaultVariants: {
+      color: "green",
+    },
+  });
+
   return (
     <>
-      <button
-        className="bg-green-600 text-gray-600 flex items-center justify-center p-2 max-w-48 w-screen rounded-lg cursor-pointer hover:bg-green-700 transition-colors duration-300 my-8"
-        {...rest}
-      >
+      <button className={button({ color, className })} {...rest}>
         {icon}
       </button>
     </>
