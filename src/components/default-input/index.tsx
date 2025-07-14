6b5875f642330c46e07ff72cbd60d2cb904b52a1
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-14 17:20:13 -0300

    refactor: update DefaultInput component styling for border focus state

--- src/components/default-input/index.tsx
+++ src/components/default-input/index.tsx
@@ -15,7 +15,7 @@ export function DefaultInput({
       <input
         id={id}
         type={type}
-        className="bg-transparent rounded-md p-2 outline-none border border-gray-600 text-white placeholder:text-gray-500 focus:border-green-700 transition-colors text-center"
+        className="bg-transparent rounded-md p-2 outline-none border focus:border border-gray-600 text-white placeholder:text-gray-500 focus:border-green-700 transition-colors text-center disabled:cursor-not-allowed disabled:bg-gray-700 disabled:border-gray-600"
         {...rest}
       />
     </>
