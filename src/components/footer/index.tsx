dd4bbdff7aa39081eb871cd9cb501b88731f1638
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-14 17:49:42 -0300

    feat: update Footer component to include a link explaining the Pomodoro technique

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,11 @@
 export function Footer() {
   return (
     <footer className="flex items-center flex-col gap-3 text-white p-4 ">
-      <p>Chronos Pomodoro &copy; {new Date().getFullYear()}.</p>
+      <a href="" className="text-gray-400 hover:underline">
+        Entenda como funciona a técnica pomodoro
+      </a>
       <p>
-        Feito com ❤️ por{" "}
+        Chronos Pomodoro &copy; {new Date().getFullYear()}. Feito com ❤️ por{" "}
         <a
           href="https://github.com/isaqu3d"
           target="_blank"
