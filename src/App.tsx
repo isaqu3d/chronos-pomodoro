75345fbe909feef5e5c751c2e36f2ed864fa9e1c
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-21 23:18:32 -0300

    feat: add footer and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { PlayCircleIcon } from "lucide-react";
+import { useState } from "react";
 import { Container } from "./components/container";
 import { CountDown } from "./components/count-down";
 import { Cycles } from "./components/cycles";
 import { DefaultButton } from "./components/default-button";
 import { DefaultInput } from "./components/default-input";
+import { Footer } from "./components/footer";
 import { Logo } from "./components/logo";
 import { Menu } from "./components/menu";
 
 export function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClickButton(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setCount((prevState) => prevState + 1);
+  }
+
   return (
-    <div className="bg-gray-950">
+    <div className="bg-gray-100 dark:bg-gray-800">
       <Container>
         <Logo />
       </Container>
@@ -37,17 +46,25 @@ export function App() {
           </div>
 
           <div className="flex justify-center items-center">
-            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.{count}</p>
           </div>
 
           <div className="flex justify-center items-center flex-col">
             <Cycles />
           </div>
 
-          <div className="flex justify-center items-center">
-            <DefaultButton icon={<PlayCircleIcon className="w-8 h-8" />} />
+          <div className="flex flex-col justify-center items-center">
+            <DefaultButton
+              onClick={handleClickButton}
+              icon={<PlayCircleIcon className="w-8 h-8" />}
+              color="green"
+            />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </div>
   );
